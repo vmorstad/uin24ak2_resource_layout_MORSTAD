1cdacc5f3425dcbb69223abcee2d5d191b2736e0
vmorstad <viljar.morstad2000@gmail.com>
2024-01-25 21:27:29 +0100

    javascript
    fungerende

--- UIN2024_coursebase-main/arbeidskrav_2/menyscript.js
+++ UIN2024_coursebase-main/arbeidskrav_2/menyscript.js
@@ -1,17 +1,39 @@
 console.log(resources)
 
-let ressurserHTML = "<h2></h2>"
+const navResource = document.getElementById('navResource')
+const contentResource = document.getElementById('contentResource')
 
-resources.map(resource => ressurserHTML +=
-    `<section id="content">
-        <h2>${resource.category}</h2>
-        <p>${resource.text}</p>
-        <nav>
-            <ul>
-                <li><a href=${resource.url}>${resource.title}</a></li>
+function giveNavContent() {
+    resources.forEach(resource => {
+        const listItem = document.createElement('li')
+        listItem.textContent = resource.category
+        listItem.addEventListener('click', () => giveContentResource(resource))
+        navResource.appendChild(listItem)
+    })
+
+    if (resources.length > 0) {
+        giveContentResource(resources[0])
+    }
+}
+
+function giveContentResource(resource) {
+    const sourcesListItems = resource.sources.map(source => `
+        <li>
+        <a href="${source.url}" target="_blank">${source.title}</a>
+        </li>`).join('') 
+
+    contentResource.innerHTML = `
+        <div class="resource-container">
+            <h2>
+                ${resource.category}
+            </h2>
+            <p>
+                ${resource.text}
+            </p>
+            <ul class="sources-list">
+                ${sourcesListItems}
             </ul>
-        </nav>
-    </section>`)
-
-const main = document.getElementsByTagName("main")
-main[0].innerHTML = ressurserHTML
+        </div>
+    `
+}
+giveNavContent()
