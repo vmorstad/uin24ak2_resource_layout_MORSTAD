a8e6bd9917d9b8641fd00114d4606890c9b225e3
vmorstad <viljar.morstad2000@gmail.com>
2024-02-13 11:56:18 +0100

    react webshop oppdatert

--- UIN2024_coursebase-main/LEGOduds_react/src/components/ProductCard.jsx
+++ UIN2024_coursebase-main/LEGOduds_react/src/components/ProductCard.jsx
@@ -1,10 +1,15 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 export default function ProductCard({category, img, title, price, setAmount, prodid, setCart, cart}){
  const [product, setProduct] = useState({
   title: title,
   price: price,
   prodid: prodid
  })
+
+ useEffect(()=>{
+  countProducts()
+ },[cart])
+
   const handleClick = ()=>{
     const exist = cart.find(item => item.prodid === product.prodid)
     setCart((prevCart) => 
@@ -13,16 +18,15 @@ export default function ProductCard({category, img, title, price, setAmount, pro
     prevCart.map(item =>  item.prodid === product.prodid ? {...item, quantity: item.quantity + 1} : item )
     :
     [...prevCart, {...product, quantity: 1}])
-    countProducts()
   }
 
   const countProducts =()=>{
-    setAmount(cart.reduce((total, item) => total + item.quantity, 1))
+    setAmount(cart.reduce((total, item) => total + item.quantity, 0))
   }
  
     return(
       <article>
-        <img src={"website_images/PROD_"+img} alt={title} />
+        <img src={"/website_images/PROD_"+img} alt={title} />
         <a href="#">{category}</a>
         <h3>{title}</h3>
         <span>Kr.{price}</span>
